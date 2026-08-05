2d911f480a93d90ad30702b0a5bbdf9452620f12
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-05 17:30:13 -0300

    chore: configure Docker environment

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Finance API");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
