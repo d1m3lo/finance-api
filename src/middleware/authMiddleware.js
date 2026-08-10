6b71a747f05d31bcd0c0ae056950aa58eb5980b6
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-10 11:25:49 -0300

    improve authMiddleware validation

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -7,16 +7,24 @@ async function authMiddleware(req, res, next) {
         if (!authHeader) return res.status(401).json({ error: "Authorization header missing" })
         if (!authHeader.startsWith("Bearer ")) return res.status(401).json({ error: "Invalid authorization format. Expected 'Bearer <token>'." })
         const token = authHeader.split(' ')[1]
+        if (!token) {
+            return res.status(401).json({
+                error: "Token missing"
+            });
+        }
         const payload = jwt.verify(token, process.env.JWT_SECRET)
         const user = await prisma.user.findUnique({ where: { id: payload.userId } })
+        if (!user) return res.status(401).json({ error: "Invalid credentials" })
         req.user = user
         next()
     } catch (err) {
-        console.log(err)
-        return res.status(500).json({ error: "Internal Server Error" })
+        return res.status(401).json({
+            error: "Invalid or expired token"
+        });
     }
-
-
 }
+
+
+
 
 module.exports = authMiddleware
