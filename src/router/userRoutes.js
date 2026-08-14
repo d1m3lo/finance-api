8116240cdc89fab0b285c32e7c21cb90d4bba577
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-14 14:38:08 -0300

    feat: add user controller for user routes

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -10,102 +10,20 @@ const saltRounds = 10
 const jwt = require("jsonwebtoken")
 const authMiddleware = require("../middleware/authMiddleware")
 const { schemaUserUpdate, schemaUserRegister, schemaUserLogin } = require("../schemas/userSchema")
+const userController = require("../controller/userController")
 
 
 
 
 
-router.post("/register", async (req, res) => {
-    try {
-        const { name, email, password } = req.body
-        const result = schemaUserRegister.safeParse({ name, email, password })
-        if (!result.success) {
-            return res.status(400).json({ error: "Validation failed" })
-        }
-        const existingEmail = await prisma.user.findUnique({
-            where: { email: result.data.email }
-        })
-        if (existingEmail) {
-            return res.status(409).json({ error: "Email already registered" })
-        }
-        const hashPassword = await bcrypt.hash(result.data.password, saltRounds)
-        await prisma.user.create({
-            data: {
-                name: result.data.name,
-                email: result.data.email,
-                password: hashPassword
-            }
-        })
-        res.status(201).json({ name: result.data.name, email: result.data.email })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({ error: "Internal Server Error" })
-    }
-})
+router.post("/register", userController.userRegister)
 
-router.post("/login", async (req, res) => {
-    try {
-        const { email, password } = req.body
-        const result = schemaUserLogin.safeParse({ email, password })
-        if (!result.success) {
-            return res.status(400).json({ error: "Validation failed" })
-        }
-        const user = await prisma.user.findUnique({
-            where: { email: result.data.email }
-        })
-        if (!user) return res.status(400).json({ error: "email or password is incorrect" })
-        const isValid = await bcrypt.compare(result.data.password, user.password)
-        if (!isValid) return res.status(400).json({ error: "email or password is incorrect" })
-        const secret = process.env.JWT_SECRET
-        const payload = { userId: user.id, name: user.name, email: user.email }
-        const token = jwt.sign(payload, secret, { expiresIn: '1h' })
-        res.status(200).json({ message: "successful login", accessToken: token })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({ error: "Internal Server Error" })
+router.post("/login", userController.userLogin)
 
-    }
-})
-
-router.patch("/", authMiddleware, async (req, res) => {
-    try {
-        const { name, email, password } = req.body
-        const data = {}
-        const result = schemaUserUpdate.safeParse({ name, email, password })
-        if (!result.success) {
-            return res.status(400).json({ error: "Validation failed" })
-        }
-        if (name !== undefined) {
-            data.name = result.data.name
-        }
-        if (email !== undefined) {
-            const existingEmail = await prisma.user.findUnique({ where: { email: result.data.email } })
-            if (existingEmail && existingEmail.id !== req.user.id) return res.status(409).json({ error: "Email already registered" })
-            data.email = result.data.email
-        }
-        if (password !== undefined) {
-            const hashPassword = await bcrypt.hash(result.data.password, saltRounds)
-            data.password = hashPassword
-        }
-        if (Object.keys(data).length === 0) return res.status(400).json({ error: "Missing fields" })
-        await prisma.user.update({ where: { id: req.user.id }, data })
-        return res.status(200).json({ message: "your data has been updated successfully" })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({ error: "Internal Server Error" })
-    }
-})
+router.patch("/", authMiddleware, userController.userUpdate)
 
 
-router.delete("/", authMiddleware, async (req, res) => {
-    try {
-        await prisma.user.delete({ where: { id: req.user.id } })
-        return res.status(200).json({ message: "User deleted successfully" })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({ error: "Internal Server Error" })
-    }
-})
+router.delete("/", authMiddleware, userController.userDelete)
 
 module.exports = router
 
