47863a7af5802c2a7e43af91c3b3519b0b50115c
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-17 16:12:11 -0300

    feat: add errorMiddleware to handle server errors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const userRoutes = require("./router/userRoutes");
 const transactionRoutes = require("./router/transactionRoutes")
 const authMiddleware = require("./middleware/authMiddleware");
+const errorMiddleware = require("./middleware/errorMiddleware");
 require("dotenv").config()
 const app = express();
 
 app.use(express.json())
 app.use("/users", userRoutes)
 app.use("/transactions", transactionRoutes)
+app.use(errorMiddleware)
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
