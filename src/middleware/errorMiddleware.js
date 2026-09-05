858e31dc8ea4c1208ec5a1df1c33beb999c36cd8
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-09-04 23:43:50 -0300

    fix: prisma import correction

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,17 +1,18 @@
-const { Prisma } = require("../lib/prisma") 
-    function errorMiddleware(err, req, res, next) {
-        console.error(err)
-        if (err instanceof Prisma.PrismaClientKnownRequestError) {
-            if (err.code === "P2002") {
-                return res.status(409).json({
-                    error: "Resource already exists"
-                })
-            }
-            if (err.code === "P2025") {
-                return res.status(404).json({ error: "Resource not found" })
-            }
+const { Prisma } = require("@prisma/client");
+
+function errorMiddleware(err, req, res, next) {
+    console.error(err)
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        if (err.code === "P2002") {
+            return res.status(409).json({
+                error: "Resource already exists"
+            })
         }
-        return res.status(500).json({ error: "Internal Server Error" })
+        if (err.code === "P2025") {
+            return res.status(404).json({ error: "Resource not found" })
+        }
     }
+    return res.status(500).json({ error: "Internal Server Error" })
+}
 
 module.exports = errorMiddleware
