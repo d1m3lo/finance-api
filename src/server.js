d7c1f7e9261570053f821899561fa2049f664301
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-09-04 22:13:42 -0300

    fix: name correction

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const userRoutes = require("./router/userRoutes");
 const transactionRoutes = require("./router/transactionRoutes")
 const errorMiddleware = require("./middleware/errorMiddleware");
-const notFoundMiddeware = require("./middleware/notFoundMiddleware");
+const notFoundMiddleware = require("./middleware/notFoundMiddleware");
 require("dotenv").config()
 const app = express();
 
 app.use(express.json())
 app.use("/users", userRoutes)
 app.use("/transactions", transactionRoutes)
-app.use(notFoundMiddeware)
+app.use(notFoundMiddleware)
 app.use(errorMiddleware)
 
 app.listen(3000, () => {
