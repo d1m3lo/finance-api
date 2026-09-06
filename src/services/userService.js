eb3b20cc61a9bdcdafd128fc688f222d94d311ee
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-09-06 00:16:50 -0300

    feat: create userService.login

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,5 @@
 
+const { InvalidCredentialsError } = require("../error/AppError")
 const prisma = require("../lib/prisma")
 const bcrypt = require("bcrypt")
 const saltRounds = 10
@@ -17,4 +18,22 @@ async function register(name, email, password) {
         email: userData.email
     }
 }
-module.exports = { register }
+
+async function login(email, password) {
+    const userData = await prisma.user.findUnique({
+        where: { email: email }
+    })
+    if (!userData) {
+        throw new InvalidCredentialsError()
+    }
+    const isValid = await bcrypt.compare(password, userData.password)
+    if (!isValid) {
+        throw new InvalidCredentialsError()
+    }
+    return {
+        id: userData.id,
+        name: userData.name,
+        email: userData.email
+    }
+}
+module.exports = { register, login }
