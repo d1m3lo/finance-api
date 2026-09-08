8c9119060636e807f82a53aeeac7015aa99271ee
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-09-08 12:04:18 -0300

    feat: create transactionService.transactionCreate

---
+++ src/services/transactionService.js
@@ -0,0 +1,13 @@
+const prisma = require("../lib/prisma")
+
+async function createTransaction(description, type, amount, userId) {
+    const transaction = await prisma.transaction.create({
+        data: {
+            description,
+            type,
+            amount,
+            userId,
+        }
+    })
+    return transaction
+}
